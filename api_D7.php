ff8386b4536e9d93628c56625c0ce3ff86c856db
Maxim <47496840+thebearcod@users.noreply.github.com>
2021-06-24 10:52:18 +0500

    add task for change SEO Title section

--- api_D7.php
+++ api_D7.php
@@ -13,6 +13,41 @@ while($arFields = $res->GetNext()) {
     print_r($arFields['ID'].' =>  '.$arFields['NAME'].' => '.$arFields['ELEMENT_CNT'].'<br>');
 }
 
-// 1. Задача: имеется список XLS с 1.ссылками/URL на раздел и 2.Значением title
+// 2. Задача: имеется список XLS с 1.ссылками/URL на раздел и 2.Значением title
 //            нужно изменить у этих разделов title на вкладке SEO
+//    Решение: сохраняем XLS в CSV закачиваем на сервер откуда будем запускать следующий скрипт php
+//             скрипт бежит по файлу выберает название конечного раздела и меняет Title
+//             !!! идеально работает только с уникальным свойством CODE !!!
 
+use Bitrix\Iblock\InheritedProperty;
+$file = 'list.csv';
+$csv = array_map('str_getcsv', file($file));
+
+foreach ($csv as $item) {
+    // разберем URL на массив, очистив от лишнего
+    $url = explode('/',str_replace('https://','',trim($item[0],'/')));
+    $IBLOCK_ID = 18; // ID нужного каталога/инфоблока
+    $arSort = array();
+    // фильтр сортируе
+    $arFilter = array("IBLOCK_ID" => $IBLOCK_ID,"CODE" => end($url) );
+    $arGroupBy = false;
+    $arSelect = Array("ID", "IBLOCK_ID", "CODE", "NAME", "PROPERTY_*");
+    $res = CIBlockSection::GetList($arSort, $arFilter, $arGroupBy, $arSelect);
+    while($arFields = $res->GetNext()) {
+        pr($arFields['ID'].' => '.end($url).' => '.$arFields["NAME"].' => '.$item[1]);
+        $ipropTemplates = new InheritedProperty\SectionTemplates($IBLOCK_ID, $arFields['ID']);
+        //Установить шаблон SEO Title для раздела
+        $ipropTemplates->set(array(
+            "SECTION_META_TITLE" => $item[1],
+        ));
+        /*
+         * некоторые названия свойств
+         * SECTION_META_TITLE
+         * SECTION_PAGE_TITLE
+         * ELEMENT_META_TITLE
+         * ELEMENT_PAGE_TITLE
+         * SECTION_META_DESCRIPTION
+         * ELEMENT_META_DESCRIPTION
+         */
+    }
+}
